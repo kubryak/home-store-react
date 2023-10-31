cc26882a0358cf7d08e7c849945f8d47ad726893
kubryak <al.lcn@yandex.ru>
2023-10-31 14:52:59 +0500

    feat: добавить иконку для закрытия увеличенной карточки

--- src/components/ShowFullItem.js
+++ src/components/ShowFullItem.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
+import deleteIcon from '../images/delete-icon.svg'
 
 export default class ShowFullItem extends Component {
+
   render() {
     return (
       <div className='full-item'>
         <div>
-          <img src={'./images/' + this.props.item.img} alt={this.props.item.title} onClick={() => this.props.onShowItem(this.props.item)} />
+          <img className='delete-icon' src={deleteIcon} onClick={() => this.props.onShowItem(this.props.item)} />
+          <img className='full-item__img' src={'./images/' + this.props.item.img} alt={this.props.item.title} />
           <h2>{this.props.item.title}</h2>
           <p>{this.props.item.description}</p>
           <strong>{this.props.item.price.toLocaleString('ru-RU')}₽</strong>
